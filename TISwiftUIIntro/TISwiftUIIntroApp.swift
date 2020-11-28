1ba43e5c64440166689c4666815a8037485a4c7f
James Valaitis <james@infinityjames.com>
2020-11-28 12:08:20 +0000

    Initial Commit

---
+++ TISwiftUIIntro/TISwiftUIIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TISwiftUIIntroApp.swift
+//  TISwiftUIIntro
+//
+//  Created by James Valaitis on 28/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct TISwiftUIIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
